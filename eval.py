c8a4b00de2efa5a9a75f13c033d533382ac59b6e
sarbot <lukas@uni-bremen.de>
2016-05-16 11:17:36 +0200

    added fft and gaussian filter

--- eval.py
+++ eval.py
@@ -11,20 +11,35 @@ import numpy as np #numeric calculations
 import matplotlib.pyplot as plt #figures and plots
 from PIL import Image as img #image objects
 import slimetools as st #own functions defined in slimetools.py
+import scipy.ndimage as ndimg #image tools like gaussian filter
 
-
+#------------------ Parameter ------------------#
+threshold_offset = -100
 #------------------ Main ------------------#
 
 image = plt.imread('first.tif') #reads image as array
+
+
+fft = np.fft.fft2(image)
+image_fft = fft
+image_fft[0,0]=0
+image_fft=np.abs(np.fft.fftshift(fft))
+
+filtered = ndimg.gaussian_filter(image, sigma=(1,1))
 grad = st.grey2grad(image) #creates x-y-gradient from image array
 limit = st.grad2threshold(image,grad) #returns threshold for this image
 binary = st.grey2bin(image,limit) #returns a binary array
 
 
 #------------------ Output ------------------#
+
 figure1 = plt.figure()
-plt.subplot(121)
+plt.subplot(221)
 image_pic = plt.imshow(image, cmap='pink')
-plt.subplot(122)
+plt.subplot(222)
 binary_pic = plt.imshow(binary, cmap='pink')
+plt.subplot(223)
+fft_pic = plt.imshow(image_fft, cmap='pink')
+plt.subplot(224)
+filtered_pic = plt.imshow(filtered, cmap='pink')
 print 'done'
--- slimetools.py
+++ slimetools.py
@@ -24,6 +24,19 @@ def findCenter(imArray):
     imCenter = np.array([imShape[0]/2,imShape[1]/2])
     return imCenter
     
+def gauss2d(shape=(3,3),sigma=0.5):
+    """
+    2D gaussian mask - should give the same result as MATLAB's
+    fspecial('gaussian',[shape],[sigma])
+    """
+    m,n = [(ss-1.)/2. for ss in shape]
+    y,x = np.ogrid[-m:m+1,-n:n+1]
+    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
     
 def grey2grad(im_array):
     """
